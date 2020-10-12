87dd8f87b48d71c63f758cd5d322a155f24107a1
crazyArtur <artur.dbsz@gmail.com>
2020-10-12 19:08:50 +0200

    setting up validation WIP

--- bootstrap/Tutorial/script.js
+++ bootstrap/Tutorial/script.js
@@ -1,3 +1,31 @@
 const name = document.getElementById('name')
-const password = document.getElementById('password')
+const email = document.getElementById('email')
 const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+// validation colors
+const green = '#4caf50';
+const red = '#f44336';
+
+function validateName() {
+    // check if empty
+    if (checkIfEmpty(name)) return;
+    // check if only letters
+}
+
+function checkIfEmpty(field) {
+    if (isEmpty(field.value.trim())) {
+        // set field invalid
+        setInvalid(field, `${field.name} must not be empty`)
+        return true;
+    } else {
+        // set field valid
+        setValid(field);
+        return false;
+    }
+}
+
+function isEmpty(value) {
+    if (value === '') return true;
+    return false;
+}
