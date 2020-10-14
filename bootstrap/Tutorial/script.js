2a907e3b3a4737ade2e7e138bdd5c39cb442d5e9
crazyArtur <artur.dbsz@gmail.com>
2020-10-14 12:33:57 +0200

    Form: validation WIP

--- bootstrap/Tutorial/script.js
+++ bootstrap/Tutorial/script.js
@@ -1,18 +1,36 @@
-const name = document.getElementById('name')
-const email = document.getElementById('email')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const number = document.getElementById('number');
+const file = document.getElementById('file');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
 
-// validation colors
+// Validation colors
 const green = '#4caf50';
 const red = '#f44336';
 
 name.onblur = validateName;
+email.onblur = validateEmail;
+number.onblur = validateNumber;
+
+// Validators
 function validateName() {
     if (checkIfEmpty(name)) return;
     if (!checkIfOnlyLetters(name)) return;
 }
 
+function validateEmail() {
+    if (checkIfEmpty(email)) return;
+    if (!containsCharacters(email)) return;
+}
+
+function validateNumber() {
+    if (checkIfEmpty(number)) return;
+    if (!meetLength(number, 4, 6)) return;
+}
+
+// Utility 
 function checkIfEmpty(field) {
     if (isEmpty(field.value.trim())) {
         // set field invalid
@@ -37,7 +55,7 @@ function setInvalid(field, message) {
 }
 
 function setValid(field, message) {
-    field.className = 'valid';
+    field.className = 'form-control is-valid';
     field.nextElementSibling.innerHTML = '';
     // field.nextElementSibling.style.color = green;
 }
@@ -48,6 +66,34 @@ function checkIfOnlyLetters(field) {
         return true;
     } else {
         setInvalid(field, `${field.name} must contain only letters`);
+        return false;
+    }
+}
+
+function meetLength(field, minLength, maxLenght) {
+    if (field.value.length >= minLength && field.value.length < maxLenght) {
+        setValid(field);
+        return true;
+    } else if (field.value.length < minLength) {
+        setInvalid(field, `${field.name} must be at least ${minLength} characters long`);
+        return false;
+    } else {
+        setInvalid(field, `${field.name} must be shorter than ${maxLenght} characters`)
+        return false;
+    }
+}
+
+function containsCharacters(field) {
+    let regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return matchWithRegEx(regEx, field, 'Must be a valid email address');
+}
+
+function matchWithRegEx(regEx, field, message) {
+    if (field.value.match(regEx)) {
+        setValid(field);
+        return true;
+    } else {
+        setInvalid(field, message);
         return false;
     }
 }
