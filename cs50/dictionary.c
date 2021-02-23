42f0bf28df51d7a9a9c461f0fb351b5a7f39119b
Artur <artur.dbsz@gmail.com>
2021-02-23 20:08:34 +0100

    fixed size

--- cs50/dictionary.c
+++ cs50/dictionary.c
@@ -91,7 +91,8 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    return word_count * sizeof(node);
+    return word_count;
+    // * sizeof(node);
 }
 
 // Unloads dictionary from memory, returning true if successful else false
