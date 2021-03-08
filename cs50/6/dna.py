065f0b90aedff3f118693a409ad0d81a3a2bd121
Artur <artur.dbsz@gmail.com>
2021-03-08 12:50:15 +0100

    SIX-Dna: WIP

--- cs50/6/dna.py
+++ cs50/6/dna.py
@@ -1,8 +1,12 @@
 from sys import argv
+import csv
 
 if(len(argv) != 3):
     print("Not enough arguments")
     exit()
 
+with open(argv[1], "r") as file:
+    spam = csv.reader(file)
+    for row in spam:
+        print(row)
 
-print("didn't exit")
