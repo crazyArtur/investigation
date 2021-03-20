f3f8fc360d19c490d1bc45efb4fef0a6b0c3c62f
Artur <artur.dbsz@gmail.com>
2021-03-20 18:19:16 +0100

    sql: houses wip

--- cs50/7/houses/import.py
+++ cs50/7/houses/import.py
@@ -1,4 +1,18 @@
 import csv
+from sys import argv
 from cs50 import SQL
 
+if(len(argv) != 2):
+    print("No file provided")
+    exit()
+
 db = SQL("sqlite:///students.db")
+
+students = []
+with open(argv[1], "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append(row)
+
+
+
