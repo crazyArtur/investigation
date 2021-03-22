51031828da237bb7a2484fc4556f1ae0ddcb2f6a
Artur <artur.dbsz@gmail.com>
2021-03-22 11:54:37 +0100

    sql: WIP

--- cs50/7/houses/import.py
+++ cs50/7/houses/import.py
@@ -12,7 +12,14 @@ students = []
 with open(argv[1], "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        students.append(row)
+        name = row["name"].split()
+        firstName = name[0]
 
+        if(len(name) < 3):
+            middleName = None
+            lastName = name[1]
+        else:
+            middleName = name[1]
+            lastName = name[2]
 
-
+        db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", firstName, middleName, lastName, row["house"], int(row["birth"]))
